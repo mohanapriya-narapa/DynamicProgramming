40b240bc80e190ab2a274dc1a355d1a8106fdbe6
kmts571011@Sai2 <mohanapriyanarapareddy@localhost>
2022-06-05 19:02:06 -0500

    climbing stairs problem

--- src/leetcode/climbingStairs.java
+++ src/leetcode/climbingStairs.java
@@ -4,6 +4,17 @@ public class climbingStairs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//dev branch
+		System.out.println(climbingStairs(6));
+	}
+
+	private static int climbingStairs(int n) {
+		// TODO Auto-generated method stub
+		int[] dp=new int[n+1];
+		dp[0]=1;dp[1]=1;
+		for(int i=2;i<=n;i++)
+		{
+			dp[i]=dp[i-1]+dp[i-2];
+		}
+		return dp[n];
 	}
 }
